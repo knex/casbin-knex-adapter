ff2f3a07d4cccecc218adc78ac223cf6cce20e54
Igor Savin <iselwin@gmail.com>
2021-01-16 20:16:01 +0200

    Add TS types

--- lib/adapter.ts
+++ lib/adapter.ts
@@ -1,12 +1,12 @@
-// ToDo implement types
-// @ts-nocheck
-
-import Knex from 'knex';
-import { Helper, Model } from 'casbin';
+import type Knex from 'knex';
+import type { Assertion, Model } from 'casbin';
+import { Helper } from 'casbin';
 
 type Policy = {
+  [key: string]: string | undefined;
+
   ptype: string;
-  v0: string;
+  v0?: string;
   v1?: string;
   v2?: string;
   v3?: string;
@@ -15,12 +15,15 @@ type Policy = {
 };
 
 export class KnexAdapter {
-  constructor(knex, tableName = 'policies') {
+  private readonly knex: Knex;
+  private readonly tableName: string;
+
+  constructor(knex: Knex, tableName = 'policies') {
     this.knex = knex;
     this.tableName = tableName;
   }
 
-  static async newAdapter(knex: Knex) {
+  static async newAdapter(knex: Knex): Promise<KnexAdapter> {
     const adapter = new KnexAdapter(knex);
     await adapter.createTable();
 
@@ -31,7 +34,7 @@ export class KnexAdapter {
     return this.knex('policies');
   }
 
-  async createTable() {
+  async createTable(): Promise<void> {
     const tableExists = await this.knex.schema.hasTable(this.tableName);
     if (!tableExists) {
       await this.knex.schema.createTable(this.tableName, (table) => {
@@ -47,52 +50,15 @@ export class KnexAdapter {
     }
   }
 
-  async dropTable() {
+  async dropTable(): Promise<void> {
     await this.knex.schema.dropTableIfExists(this.tableName);
   }
 
-  async close() {
+  async close(): Promise<void> {
     await this.knex.destroy();
   }
 
-  createFilteredPolicy(ptype, fieldIndex, ...fieldValues) {
-    const filteredPolicy = {};
-
-    filteredPolicy.ptype = ptype;
-
-    if (fieldIndex <= 0 && 0 < fieldIndex + fieldValues.length) {
-      filteredPolicy.v0 = fieldValues[0 - fieldIndex];
-    }
-    if (fieldIndex <= 1 && 1 < fieldIndex + fieldValues.length) {
-      filteredPolicy.v1 = fieldValues[1 - fieldIndex];
-    }
-    if (fieldIndex <= 2 && 2 < fieldIndex + fieldValues.length) {
-      filteredPolicy.v2 = fieldValues[2 - fieldIndex];
-    }
-    if (fieldIndex <= 3 && 3 < fieldIndex + fieldValues.length) {
-      filteredPolicy.v3 = fieldValues[3 - fieldIndex];
-    }
-    if (fieldIndex <= 4 && 4 < fieldIndex + fieldValues.length) {
-      filteredPolicy.v4 = fieldValues[4 - fieldIndex];
-    }
-    if (fieldIndex <= 5 && 5 < fieldIndex + fieldValues.length) {
-      filteredPolicy.v5 = fieldValues[5 - fieldIndex];
-    }
-
-    return filteredPolicy;
-  }
-
-  createPoliciesFromAstMap(astMap) {
-    const policies = [];
-    for (const [ptype, ast] of astMap) {
-      for (const rule of ast.policy) {
-        policies.push(this.createPolicy(ptype, rule));
-      }
-    }
-    return policies;
-  }
-
-  async loadPolicy(model) {
+  async loadPolicy(model: Model): Promise<void> {
     const policies = await this.policiesTable.select();
 
     for (const policy of policies) {
@@ -100,33 +66,50 @@ export class KnexAdapter {
     }
   }
 
-  async savePolicy(model) {
+  async savePolicy(model: Model): Promise<void> {
     await this.dropTable();
     await this.createTable();
 
     await this.policiesTable.insert([
-      ...this.createPoliciesFromAstMap(model.model.get('p')),
-      ...this.createPoliciesFromAstMap(model.model.get('g')),
+      ...createPoliciesFromAstMap(model.model.get('p')),
+      ...createPoliciesFromAstMap(model.model.get('g')),
     ]);
   }
 
-  async addPolicy(sec: string, ptype: string, rule: readonly string[]) {
+  async addPolicy(
+    sec: string,
+    ptype: string,
+    rule: readonly string[]
+  ): Promise<void> {
     const policy = createPolicy(ptype, rule);
     await this.policiesTable.insert(policy);
   }
 
-  async removePolicy(sec, ...args) {
-    const policy = this.createPolicy(...args);
+  async removePolicy(
+    sec: string,
+    ptype: string,
+    rule: readonly string[]
+  ): Promise<void> {
+    const policy = createPolicy(ptype, rule);
     await this.policiesTable.delete().where(policy);
   }
 
-  async removeFilteredPolicy(sec, ...args) {
-    const filteredPolicy = this.createFilteredPolicy(...args);
+  async removeFilteredPolicy(
+    sec: string,
+    ptype: string,
+    fieldIndex: number,
+    ...fieldValues: string[]
+  ): Promise<void> {
+    const filteredPolicy = createFilteredPolicy(
+      ptype,
+      fieldIndex,
+      ...fieldValues
+    );
     await this.policiesTable.delete().where(filteredPolicy);
   }
 }
 
-function loadPolicyLine(policy: Policy, model: Model) {
+function loadPolicyLine(policy: Policy, model: Model): void {
   const policyLine =
     policy.ptype +
     ', ' +
@@ -148,10 +131,55 @@ function createPolicy(ptype: string, rule: readonly string[]): Policy {
   }
 
   return rule.reduce(
-    (acc, value, index) => {
+    (acc: Policy, value, index) => {
       acc[`v${index}`] = rule[index];
       return acc;
     },
     { ptype }
   );
+}
+
+function createPoliciesFromAstMap(
+  astMap: Map<string, Assertion> | undefined
+): Policy[] {
+  const policies: Policy[] = [];
+  if (!astMap) {
+    return policies;
+  }
+
+  for (const [ptype, ast] of astMap) {
+    for (const rule of ast.policy) {
+      policies.push(createPolicy(ptype, rule));
+    }
+  }
+  return policies;
+}
+
+function createFilteredPolicy(
+  ptype: string,
+  fieldIndex: number,
+  ...fieldValues: string[]
+): Policy {
+  const filteredPolicy: Policy = { ptype };
+
+  if (fieldIndex <= 0 && 0 < fieldIndex + fieldValues.length) {
+    filteredPolicy.v0 = fieldValues[0 - fieldIndex];
+  }
+  if (fieldIndex <= 1 && 1 < fieldIndex + fieldValues.length) {
+    filteredPolicy.v1 = fieldValues[1 - fieldIndex];
+  }
+  if (fieldIndex <= 2 && 2 < fieldIndex + fieldValues.length) {
+    filteredPolicy.v2 = fieldValues[2 - fieldIndex];
+  }
+  if (fieldIndex <= 3 && 3 < fieldIndex + fieldValues.length) {
+    filteredPolicy.v3 = fieldValues[3 - fieldIndex];
+  }
+  if (fieldIndex <= 4 && 4 < fieldIndex + fieldValues.length) {
+    filteredPolicy.v4 = fieldValues[4 - fieldIndex];
+  }
+  if (fieldIndex <= 5 && 5 < fieldIndex + fieldValues.length) {
+    filteredPolicy.v5 = fieldValues[5 - fieldIndex];
+  }
+
+  return filteredPolicy;
 }
